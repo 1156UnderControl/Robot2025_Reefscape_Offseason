a7f1def48dab4c24a90d2c9e00b3ca889038f0d5
KlausKollerKroeff <kroeffk@icloud.com>
2025-08-29 13:31:15 -0300

    feat: scorer coral commands

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,8 +3,11 @@ package frc.robot;
 import com.pathplanner.lib.auto.AutoBuilder;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.commands.States.SwerveTeleopState;
+import frc.robot.constants.FieldConstants.ReefLevel;
 import frc.robot.joysticks.DriverController;
+import frc.robot.joysticks.OperatorController;
 import frc.robot.subsystems.climber.ClimberSubsystem;
 import frc.robot.subsystems.scorer.ScorerSubsystem;
 import frc.robot.subsystems.swerve.SwerveSubsystem;
@@ -14,6 +17,7 @@ import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 public class RobotContainer {
 
   private final LoggedDashboardChooser<Command> autoChooser;
+  private final OperatorController operatorController;
   private final DriverController driverController;
 
   private final SwerveSubsystem swerve;
@@ -24,13 +28,45 @@ public class RobotContainer {
     this.swerve = new SwerveSubsystem();
     this.scorer = ScorerSubsystem.getInstance();
     this.climber = ClimberSubsystem.getInstance();
+
     this.driverController = DriverController.getInstance();
+    this.operatorController = OperatorController.getInstance();
+
     autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
+
     this.swerve.setDefaultCommand(Commands.run(() -> new SwerveTeleopState(), this.swerve));
+    this.scorer.setDefaultCommand(Commands.run(() -> this.scorer.moveScorerToDefaultPosition(), this.scorer));
     this.configureButtonBindings();
   }
 
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    this.operatorController.reefL1()
+      .onTrue(new InstantCommand(() -> { 
+        this.scorer.setTargetCoralLevel(ReefLevel.L1);
+        this.scorer.setManualScoreCoral(true);
+      }));
+    
+    this.operatorController.reefL2()
+      .onTrue(new InstantCommand(() -> { 
+        this.scorer.setTargetCoralLevel(ReefLevel.L2);
+        this.scorer.setManualScoreCoral(true);
+      }));
+
+    this.operatorController.reefL3()
+      .onTrue(new InstantCommand(() -> { 
+        this.scorer.setTargetCoralLevel(ReefLevel.L3);
+        this.scorer.setManualScoreCoral(true);
+      }));
+
+    this.operatorController.reefL4()
+      .onTrue(new InstantCommand(() -> { 
+        this.scorer.setTargetCoralLevel(ReefLevel.L4);
+        this.scorer.setManualScoreCoral(true);
+      }));
+
+    this.operatorController.prepareToScore()
+      .onTrue(Commands.run(() -> this.scorer.prepareToScoreCoral(), scorer));
+  }
 
   public Command getAutonomousCommand() {
     return autoChooser.get();
