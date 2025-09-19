ceac898638ca6ba5767ccce7c5966bf4f2d439bc
KlausKollerKroeff <kroeffk@icloud.com>
2025-09-19 10:54:15 -0300

    fix: wrong sequential command

--- src/main/java/frc/robot/commands/States/ScoreObjectPosition.java
+++ src/main/java/frc/robot/commands/States/ScoreObjectPosition.java
@@ -3,24 +3,19 @@ package frc.robot.commands.States;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.Scorer.MoveScorerToPrepareScore;
-import frc.robot.commands.Scorer.MoveScorerToScoreCoral;
+import frc.robot.commands.Scorer.MoveScorerToScorePosition;
 import frc.robot.joysticks.OperatorController;
-import frc.robot.subsystems.Intake.IntakeSubsystem;
 import frc.robot.subsystems.scorer.ScorerSubsystem;
 
 public class ScoreObjectPosition extends SequentialCommandGroup {
     OperatorController operatorController = OperatorController.getInstance();
     ScorerSubsystem scorer = ScorerSubsystem.getInstance();
 
-public ScoreObjectPosition (ScorerSubsystem scorer){ 
-
-    addCommands(
-        new Movescorertoprep()
-            .until(() -> this.scorer.isElevatorAtTargetPosition() && this.scorer.isPivotAtTargetPosition()),
-                Commands.waitUntil(operatorController.scoreObject())
-                .andThen(new MoveScorerToScoreCoral())
-                .until(operatorController.goToDefaultPosition())
-                );
-        
-            }
-        }
+    public ScoreObjectPosition (ScorerSubsystem scorer){ 
+        addCommands(
+            new MoveScorerToPrepareScore(this.scorer),
+            Commands.waitUntil(operatorController.scoreObject())
+                .andThen(new MoveScorerToScorePosition(this.scorer))
+        );
+    }
+}
