f18a1ab3b0b0f56f652398cf45dbfcb10d514c9e
Carvalho <75193903+CarvalhoJeo@users.noreply.github.com>
2025-09-28 00:43:36 -0300

    chore: clean up useless imports

--- src/main/java/frc/robot/commands/States/CollectCoralPosition.java
+++ src/main/java/frc/robot/commands/States/CollectCoralPosition.java
@@ -1,12 +1,8 @@
 package frc.robot.commands.States;
 
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.ConditionalCommand;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.Intake.MoveIntakeToCollectPosition;
 import frc.robot.commands.Scorer.CollectCoralFromIndexer;
-import frc.robot.joysticks.OperatorController;
 import frc.robot.subsystems.Intake.IntakeSubsystem;
 import frc.robot.subsystems.scorer.ScorerSubsystem;
 
