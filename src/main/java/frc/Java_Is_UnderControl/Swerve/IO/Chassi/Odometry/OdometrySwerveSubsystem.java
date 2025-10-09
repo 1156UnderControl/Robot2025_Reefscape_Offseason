40076d54fdee9d7013bc593b7f07da844850a392
KlausKollerKroeff <kroeffk@icloud.com>
2025-10-09 18:10:07 -0300

    feat: now logging all odometryswervesubsystem class with advantageKit

--- src/main/java/frc/Java_Is_UnderControl/Swerve/IO/Chassi/Odometry/OdometrySwerveSubsystem.java
+++ src/main/java/frc/Java_Is_UnderControl/Swerve/IO/Chassi/Odometry/OdometrySwerveSubsystem.java
@@ -1,8 +1,7 @@
 package frc.Java_Is_UnderControl.Swerve.IO.Chassi.Odometry;
 
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
 import java.util.Optional;
+import org.littletonrobotics.junction.Logger;
 
 import com.ctre.phoenix6.swerve.SwerveDrivetrainConstants;
 import com.ctre.phoenix6.swerve.SwerveModuleConstants;
@@ -12,21 +11,14 @@ import com.pathplanner.lib.path.PathPlannerPath;
 
 import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import frc.Java_Is_UnderControl.Logging.EnhancedLoggers.CustomBooleanLogger;
-import frc.Java_Is_UnderControl.Logging.EnhancedLoggers.CustomDoubleLogger;
-import frc.Java_Is_UnderControl.Logging.EnhancedLoggers.CustomPose2dLogger;
-import frc.Java_Is_UnderControl.Logging.EnhancedLoggers.CustomStringLogger;
 import frc.Java_Is_UnderControl.Swerve.Configs.OdometryEnabledSwerveConfig;
 import frc.Java_Is_UnderControl.Swerve.IO.Chassi.Base.BaseSwerveSubsystem;
 import frc.Java_Is_UnderControl.Util.AllianceFlipUtil;
@@ -54,24 +46,6 @@ public abstract class OdometrySwerveSubsystem extends BaseSwerveSubsystem {
   private Pose2d targetPose;
 
   private Pose2d targetAimPose;
-
-  private CustomDoubleLogger targetVyDriveToPoseLogger = new CustomDoubleLogger("/SwerveSubsystem/targetVyDriveToPose");
-
-  private CustomDoubleLogger targetVxDriveToPoseLogger = new CustomDoubleLogger("/SwerveSubsystem/targetVxDriveToPose");
-
-  private CustomPose2dLogger targetPoseLogger = new CustomPose2dLogger("/SwerveSubsystem/TargetPose");
-
-  private CustomPose2dLogger targetAimPositionLogger = new CustomPose2dLogger("/SwerveSubsystem/TargetAimPosition");
-
-  private CustomBooleanLogger isAtTargetXAxisLogger = new CustomBooleanLogger("/SwerveSubsystem/IsAtTargetXAxis");
-
-  private CustomBooleanLogger isAtTargetYAxisLogger = new CustomBooleanLogger("/SwerveSubsystem/IsAtTargetYAxis");
-
-  private CustomBooleanLogger isAtTargetPoseLogger = new CustomBooleanLogger("/SwerveSubsystem/IsAtTargetPose");
-
-  private CustomPose2dLogger poseVisionLogger = new CustomPose2dLogger("/SwerveSubsystem/PoseVision");
-
-  private CustomStringLogger poseEstimatorNameLogger = new CustomStringLogger("/SwerveSubsystem/PoseEstimatorName");
 
   // Create the constraints to use while pathfinding. The constraints defined in
   // the path will only be used for the path.
@@ -139,10 +113,10 @@ public abstract class OdometrySwerveSubsystem extends BaseSwerveSubsystem {
     if (possibleEstimatedPose.isPresent()) {
       PoseEstimation estimatedPose = possibleEstimatedPose.get();
       Pose2d poseVision = estimatedPose.estimatedPose.toPose2d();
-      this.poseVisionLogger.appendRadians(poseVision);
+      Logger.recordOutput("Subsystems/Swerve/Odometry/poseVision", poseVision);
       this.setVisionStdDev(estimatedPose.visionStdDev);
       this.addVisionMeasurement(poseVision, estimatedPose.timestampSeconds);
-      this.poseEstimatorNameLogger.append(poseEstimator.getEstimatorName());
+      Logger.recordOutput("Subsystems/Swerve/Odometry/poseEstimatorName", poseEstimator.getEstimatorName());
     }
   }
 
@@ -207,8 +181,8 @@ public abstract class OdometrySwerveSubsystem extends BaseSwerveSubsystem {
       targetXVelocity = targetXVelocity * conversionFactor;
       targetYVelocity = targetYVelocity * conversionFactor;
     }
-    this.targetVxDriveToPoseLogger.append(targetXVelocity);
-    this.targetVyDriveToPoseLogger.append(targetYVelocity);
+    Logger.recordOutput("Subsystems/Swerve/Odometry/targetXVelocity", targetXVelocity);
+    Logger.recordOutput("Subsystems/Swerve/Odometry/targetYVelocity", targetYVelocity);
     ChassisSpeeds desiredSpeeds = new ChassisSpeeds(targetXVelocity, targetYVelocity, 0);
     this.driveFieldOrientedLockedAngle(desiredSpeeds, targetPose.getRotation());
     this.targetPose = targetPose;
@@ -222,9 +196,9 @@ public abstract class OdometrySwerveSubsystem extends BaseSwerveSubsystem {
     boolean isAtYAxis = yAxisDistance <= yAxisToleranceMeters;
     boolean isAtAngle = this.isAtTargetHeading(angleToleranceDegrees);
     boolean isAtTargetPose = isAtXAxis && isAtYAxis && isAtAngle;
-    this.isAtTargetXAxisLogger.append(isAtXAxis);
-    this.isAtTargetYAxisLogger.append(isAtYAxis);
-    this.isAtTargetPoseLogger.append(isAtTargetPose);
+    Logger.recordOutput("Subsystems/Swerve/Odometry/isAtXAxis", isAtXAxis);
+    Logger.recordOutput("Subsystems/Swerve/Odometry/isAtYAxis", isAtYAxis);
+    Logger.recordOutput("Subsystems/Swerve/Odometry/isAtTargetPose", isAtTargetPose);
     return isAtTargetPose;
   }
 
@@ -234,9 +208,9 @@ public abstract class OdometrySwerveSubsystem extends BaseSwerveSubsystem {
     boolean isAtXAxis = xAxisDistance <= xAxisToleranceMeters;
     boolean isAtYAxis = yAxisDistance <= yAxisToleranceMeters;
     boolean isAtTargetPose = isAtXAxis && isAtYAxis;
-    this.isAtTargetXAxisLogger.append(isAtXAxis);
-    this.isAtTargetYAxisLogger.append(isAtYAxis);
-    this.isAtTargetPoseLogger.append(isAtTargetPose);
+    Logger.recordOutput("Subsystems/Swerve/Odometry/isAtXAxis", isAtXAxis);
+    Logger.recordOutput("Subsystems/Swerve/Odometry/isAtYAxis", isAtYAxis);
+    Logger.recordOutput("Subsystems/Swerve/Odometry/isAtTargetPose", isAtTargetPose);
     return isAtTargetPose;
   }
 
@@ -246,9 +220,9 @@ public abstract class OdometrySwerveSubsystem extends BaseSwerveSubsystem {
     boolean isAtXAxis = xAxisDistance <= xAxisToleranceMeters;
     boolean isAtYAxis = yAxisDistance <= yAxisToleranceMeters;
     boolean isAtTargetPose = isAtXAxis && isAtYAxis;
-    this.isAtTargetXAxisLogger.append(isAtXAxis);
-    this.isAtTargetYAxisLogger.append(isAtYAxis);
-    this.isAtTargetPoseLogger.append(isAtTargetPose);
+    Logger.recordOutput("Subsystems/Swerve/Odometry/isAtXAxis", isAtXAxis);
+    Logger.recordOutput("Subsystems/Swerve/Odometry/isAtYAxis", isAtYAxis);
+    Logger.recordOutput("Subsystems/Swerve/Odometry/isAtTargetPose", isAtTargetPose);
     return isAtTargetPose;
   }
 
@@ -275,89 +249,6 @@ public abstract class OdometrySwerveSubsystem extends BaseSwerveSubsystem {
     return pathfindingCommand;
   }
 
-  /** Measures the robot's wheel radius by spinning in a circle. */
-  public Command wheelRadiusCharacterization() {
-    SlewRateLimiter limiter = new SlewRateLimiter(0.25);
-    WheelRadiusCharacterizationState state = new WheelRadiusCharacterizationState();
-    return Commands.parallel(
-        // Drive control sequence
-        Commands.sequence(
-            // Reset acceleration limiter
-            Commands.runOnce(
-                () -> {
-                  limiter.reset(0.0);
-                }),
-
-            // Turn in place, accelerating up to full speed
-            Commands.run(
-                () -> {
-                  double speed = limiter.calculate(5);
-                  this.driveRobotOriented(new ChassisSpeeds(0.0, 0.0, speed));
-                  System.out.println(this.driveBaseRadius);
-                },
-                this)),
-
-        // Measurement sequence
-        Commands.sequence(
-            // Wait for modules to fully orient before starting measurement
-            Commands.wait(1.0),
-
-            // Record starting measurement
-            Commands.runOnce(
-                () -> {
-                  state.positions = this.getWheelRadiusCharacterizationPositions();
-                  state.lastAngle = this.getHeading();
-                  state.gyroDelta = 0.0;
-                }),
-
-            // Update gyro delta
-            Commands.run(
-                () -> {
-                  var rotation = this.getHeading();
-                  state.gyroDelta += Math.abs(rotation.minus(state.lastAngle).getRadians());
-                  state.lastAngle = rotation;
-                })
-
-                // When cancelled, calculate and print results
-                .finallyDo(
-                    () -> {
-                      double[] positions = super.getWheelRadiusCharacterizationPositions();
-                      double wheelDelta = 0.0;
-                      for (int i = 0; i < 4; i++) {
-                        wheelDelta += Math.abs(positions[i] - state.positions[i]) / 4.0;
-                      }
-                      double wheelRadius = (state.gyroDelta * driveBaseRadius) / wheelDelta;
-
-                      NumberFormat formatter = new DecimalFormat("#0.000");
-                      System.out.println(
-                          "********** Wheel Radius Characterization Results **********");
-                      System.out.println(
-                          "\tWheel Delta: " + formatter.format(wheelDelta) + " radians");
-                      System.out.println(
-                          "\tGyro Delta: " + formatter.format(state.gyroDelta) + " radians");
-                      System.out.println(
-                          "\tWheel Radius: "
-                              + formatter.format(wheelRadius)
-                              + " meters, "
-                              + formatter.format(Units.metersToInches(wheelRadius))
-                              + " inches");
-                    })));
-  }
-
-  protected class WheelRadiusCharacterizationState {
-    double[] positions = new double[4];
-    Rotation2d lastAngle = new Rotation2d();
-    double gyroDelta = 0.0;
-  }
-
-  public double[] getWheelRadiusCharacterizationPositions() {
-    double[] values = new double[4];
-    for (int i = 0; i < 4; i++) {
-      values[i] = this.getState().ModuleStates[i].angle.getRadians();
-    }
-    return values;
-  }
-
   @Override
   public void periodic() {
     this.updateLimelightRequiredValues();
@@ -372,8 +263,7 @@ public abstract class OdometrySwerveSubsystem extends BaseSwerveSubsystem {
   }
 
   private void updateOdometrySwerveLogs() {
-    this.targetAimPositionLogger.appendRadians(targetAimPose);
-    this.targetPoseLogger.appendRadians(this.targetPose);
+    Logger.recordOutput("Subsystems/Swerve/Odometry/targetAimPose", targetAimPose);
+    Logger.recordOutput("Subsystems/Swerve/Odometry/targetPose", targetPose);
   }
-
 }
--- src/main/java/frc/Java_Is_UnderControl/Swerve/IO/Chassi/Odometry/OdometrySwerveSubsystemIO.java
+++
@@ -1,5 +0,0 @@
-package frc.Java_Is_UnderControl.Swerve.IO.Chassi.Odometry;
-
-public class OdometrySwerveSubsystemIO {
-    
-}
